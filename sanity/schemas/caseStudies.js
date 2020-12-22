7577cc9cbf350767c2313334633e1b24c9e52ad5
efenny <e.freundorfer@gmail.com>
2020-12-21 22:29:46 -0500

    init cm

---
+++ sanity/schemas/caseStudies.js
@@ -0,0 +1,17 @@
+import { MdDesktopMac } from 'react-icons/md';
+
+const CaseStudies = {
+  title: 'Case Studies',
+  name: 'case-studies',
+  type: 'document',
+  icon: MdDesktopMac,
+  fields: [
+    {
+      title: 'Name',
+      name: 'name',
+      type: 'string',
+    },
+  ],
+};
+
+export default CaseStudies;
---
+++ sanity/schemas/pastWork.js
@@ -0,0 +1,17 @@
+import { MdComputer } from 'react-icons/md';
+
+const PastWork = {
+  title: 'Past Work',
+  name: 'past-work',
+  type: 'document',
+  icon: MdComputer,
+  fields: [
+    {
+      title: 'Name',
+      name: 'name',
+      type: 'string',
+    },
+  ],
+};
+
+export default PastWork;
