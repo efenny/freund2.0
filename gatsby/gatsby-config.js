80886b94d7514a90ccb244da40398f5db03bd5c2
efenny <e.freundorfer@gmail.com>
2021-01-10 18:50:22 -0500

    changes descriptions

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
   siteMetadata: {
     title: `Erik Freundorfer`,
     jobDescription: `Frontend Developer/Designer`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `Frontend Developer with a passion for creating beautiful web experiences. Currently learning WebGL.`,
     author: `Erik Freundorfer`,
   },
   plugins: [
